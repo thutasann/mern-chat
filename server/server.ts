068df60e56d9af010138af178a20e92a7c5a7e82
thutasann <thutasann2002@gmail.com>
2023-03-13 11:08:20 +0630

    chore: setup

---
+++ server/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("API is running")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is Running on PORT : ${PORT}`)
+});
