cb302e0a49a5e2f806110aee72b773f96a6fa835
thutasann <thutasann2002@gmail.com>
2023-03-14 12:57:09 +0630

    chore: prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,18 @@ import ChatPage from './pages/chat';
 import HomePage from './pages/home';
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<HomePage/>} />
-      <Route path='/chats' element={<ChatPage/>}/>
-    </Routes>
-  )
-}
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={<HomePage />}
+			/>
+			<Route
+				path="/chats"
+				element={<ChatPage />}
+			/>
+		</Routes>
+	);
+};
 
-export default App
+export default App;
