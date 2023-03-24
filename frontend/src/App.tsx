528d9703f7b9c3f96a2c09017dd3f41f283aae7c
thutasann <thutasann2002@gmail.com>
2023-03-24 16:08:13 +0630

    feat: test my own npm package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import ChatPage from './pages/chat';
 import HomePage from './pages/home';
+import { useFetch } from 'baroque-hooks';
 
 const App = () => {
+	const { data, loading, error } = useFetch('');
+
 	return (
 		<Routes>
 			<Route
