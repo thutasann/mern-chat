70b979bd31a5d177b58c1861a3e383ade70717a6
thutasann <thutasann2002@gmail.com>
2023-03-26 00:00:34 +0630

    ui: canvas page

---
+++ frontend/src/components/canvas-forms/modal.tsx
@@ -0,0 +1,84 @@
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
+type Props = {
+	isOpen: any;
+	setIsOpen: any;
+	users: any[];
+	user: any;
+};
+
+function Modal({ isOpen, setIsOpen, users, user }: Props) {
+	const closeModal = () => {
+		setIsOpen(false);
+	};
+
+	return (
+		<div>
+			<Transition
+				appear
+				show={isOpen}
+				as={Fragment}
+			>
+				<Dialog
+					as="div"
+					className="relative z-10"
+					onClose={closeModal}
+				>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div className="fixed inset-0 backdrop-filter backdrop-blur-sm bg-opacity-10" />
+					</Transition.Child>
+
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex items-center justify-center min-h-full p-4 text-center">
+							<Transition.Child
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
+							>
+								<Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-gray-50 border border-gray-100 shadow-xl rounded-2xl">
+									<Dialog.Title
+										as="h3"
+										className="flex items-center justify-center 
+                                        space-x-2 text-[18px]  leading-6 text-gray-700 font-bold"
+									>
+										All Joiners
+									</Dialog.Title>
+									<div className="mt-5">
+										<ul>
+											{users?.map((u, index) => (
+												<li
+													key={index}
+													className="text-[15px] text-gray-800 my-3"
+												>
+													🤖 {u?.name}
+													<span className="font-bold ml-1">
+														{user && user?.userId === u?.userId && '(You)'}
+													</span>
+												</li>
+											))}
+										</ul>
+									</div>
+								</Dialog.Panel>
+							</Transition.Child>
+						</div>
+					</div>
+				</Dialog>
+			</Transition>
+		</div>
+	);
+}
+
+export default Modal;
--- frontend/src/pages/canvas-page.tsx
+++ frontend/src/pages/canvas-page.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Socket } from 'socket.io-client';
+import Modal from '../components/canvas-forms/modal';
+import { ChatState } from '../context/chat-provider';
+import SlideDrawer from '../miscellaneous/Drawer';
+import { SocketNames, toolType } from '../types';
 
 interface CanvasPageProps {
 	user: any;
@@ -8,7 +12,43 @@ interface CanvasPageProps {
 }
 
 const CanvasPage: React.FC<CanvasPageProps> = ({ user, users, socket }) => {
-	return <div>CanvasPage</div>;
+	const { user: loggedInUser } = ChatState();
+	const cavasRef = useRef<HTMLCanvasElement>(null);
+	const ctxRef = useRef<any>(null);
+	const [tool, setTool] = useState<toolType>('pencil');
+	const [color, setColor] = useState<string>('black');
+	const [elements, setElements] = useState<any[]>([]);
+	const [history, setHistory] = useState<any[]>([]);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	useEffect(() => {
+		socket.emit<SocketNames>('userLeft', user);
+	}, []);
+
+	const openModal = () => {
+		setIsOpen(true);
+	};
+
+	return (
+		<div>
+			{loggedInUser && <SlideDrawer />}
+			<div className="mainWrapper">
+				<h1 className="font-semibold text-2xl uppercase">Canvas Drawing</h1>
+				<button
+					className="joinersBtn mt-3"
+					onClick={openModal}
+				>
+					Joiners : {users?.length || 0}
+				</button>
+				<Modal
+					isOpen={isOpen}
+					setIsOpen={setIsOpen}
+					users={users}
+					user={user}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default CanvasPage;
