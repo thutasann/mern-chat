a0845e534f08e46565a9c757e6c8242e086f9c1b
thutasann <thutasann2002@gmail.com>
2023-03-30 14:42:11 +0630

    feat: redux setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,16 +5,20 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.min.css';
+import { Provider } from 'react-redux';
+import Store from './store/store';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
 root.render(
-	<ChakraProvider>
-		<Router>
-			<ChatProvider>
-				<App />
-			</ChatProvider>
-		</Router>
-	</ChakraProvider>,
+	<Provider store={Store}>
+		<ChakraProvider>
+			<Router>
+				<ChatProvider>
+					<App />
+				</ChatProvider>
+			</Router>
+		</ChakraProvider>
+	</Provider>,
 );
---
+++ frontend/src/store/text-box-store.ts
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const textboxSlice = createSlice({
+	name: 'textbox',
+	initialState: {
+		height: 400,
+		width: 400,
+		B_color: 'white',
+		pencolor: 'black',
+		stroke: 2,
+		canvaVal: null,
+	},
+	reducers: {
+		setHeight(state, action) {
+			state.height = action.payload;
+		},
+		setWidth(state, action) {
+			state.width = action.payload;
+		},
+		setColor(state, action) {
+			state.B_color = action.payload;
+		},
+		setPencolor(state, action) {
+			state.pencolor = action.payload;
+		},
+		setStroke(state, action) {
+			state.stroke = action.payload;
+		},
+		setCanva(state, action) {
+			state.canvaVal = action.payload;
+		},
+	},
+});
+
+export const actions = textboxSlice.actions;
+export default textboxSlice;
