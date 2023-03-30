96a77f0c85e252e7de62e0ae91456afb48a2d516
thutasann <thutasann2002@gmail.com>
2023-03-30 14:45:59 +0630

    chore: refactor

--- frontend/src/components/canvas-forms/whiteboard.tsx
+++ frontend/src/components/canvas-forms/whiteboard.tsx
@@ -2,18 +2,7 @@ import React from 'react';
 import { Socket } from 'socket.io-client';
 import { RoomTypes, toolType } from '../../types';
 
-interface WhiteboardProps {
-	canvasRef: any;
-	ctxRef: any;
-	elements: any[];
-	setElements: any;
-	tool: toolType;
-	color: string;
-	user: RoomTypes;
-	socket: Socket;
-}
-
-const WhiteBoard: React.FC<WhiteboardProps> = () => {
+const WhiteBoard: React.FC = () => {
 	return <div></div>;
 };
 
