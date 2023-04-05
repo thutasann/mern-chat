fec43a950a8405ac06bb788a843168f6fdac7a3b
thutasann <thutasann2002@gmail.com>
2023-04-05 09:14:29 +0630

    ui: tic playground

--- frontend/src/components/tic-tac/tic-score.tsx
+++ frontend/src/components/tic-tac/tic-score.tsx
@@ -12,11 +12,10 @@ const TicScore: React.FC<ITicScore> = ({
 	oponentScore,
 }) => {
 	return (
-		<div className="flex items-center gap-3 mt-4">
+		<div className="flex items-center gap-5 mt-4">
 			<div className="score bg-gradient-to-tr from-teal-400 to-teal-600">
 				<p>You</p> <h3>{myScore}</h3>
 			</div>
-			<h3 className="vsText">VS</h3>
 			<div className="score bg-gradient-to-r from-rose-400 to-red-500">
 				<p>{oponentName || 'Oponent'} </p> <h3>{oponentScore}</h3>
 			</div>
